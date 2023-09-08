5aa3e7e887107be573f1ced6b898a732972890de
Salma394 <salmaibrahim813@gmail.com>
2023-09-08 12:39:49 +0100

    adding Variables Task

---
+++ VariablesExercise/src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+//        String firstName = "Salma";
+//        String lastName = "Ibrahim";
+//        String Name = firstName + " " + lastName;
+//        System.out.println(Name);
+            int num1 = 12;
+            int num2 = 8;
+            System.out.println(num1+num2);
+            int result = num1/ num2;
+            System.out.println(result);
+            int num3 = 8;
+            System.out.println(num3++);
+            System.out.println(num3);
+            System.out.println(++num3);
+
+
+        }
+    }
